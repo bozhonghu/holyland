ec01c907b9b4195d1da777887c30b83c4ebee931
bozhonghu <bozhonghu@gmail.com>
2019-05-20 00:13:02 -0700

    db

--- model/db.js
+++ model/db.js
@@ -1,2 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/myDB');
+//mongoose.connect('mongodb://localhost/myDB');
+mongoose.connect('mongodb://milfhunter69:milfsquad5@ds143326.mlab.com:43326/heroku_3n0j7x7x')
